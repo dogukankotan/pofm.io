7b32ba65d5e1f5ffe4614f644882d77eca3b5bc6
Doğukan Kotan <dogukankotan@std.sehir.edu.tr>
2016-05-10 17:51:40 +0300

    destroy added and fix args

--- src/iodit.h
+++ src/iodit.h
@@ -1,59 +1,85 @@
-void append();
-void insert();
-void destroy();
-void show();
+void append(char *filename);
+
+void insert(char *filename);
+
+void destroy(char *filename);
+
+void show(char *filename);
+
 void help();
 
 
 void help() {
-	puts("Usage:");
-	puts("-a or --append:");
-	puts("\tLinux or Mac:");
-	puts("\t\t./iodit filename -a text");
-	puts("\tWindows:");
-	puts("\t\tiodit.exe filename -a text");
-	puts("\tExample: textfile.txt -a hello world");
+    puts("Usage:");
+    puts("-a or --append:");
+    puts("\tLinux or Mac:");
+    puts("\t\t./iodit filename -a text");
+    puts("\tWindows:");
+    puts("\t\tiodit.exe filename -a text");
+    puts("\tExample: textfile.txt -a hello world");
 
-	puts("-i or --insert:");
-	puts("\tLinux or Mac:");
-	puts("\t\t./iodit filename -i position text");
-	puts("\tWindows:");
-	puts("\t\tiodit.exe filename -i position text");
-	puts("\tExample: textfile.txt -i 51 hello world");
+    puts("-i or --insert:");
+    puts("\tLinux or Mac:");
+    puts("\t\t./iodit filename -i position text");
+    puts("\tWindows:");
+    puts("\t\tiodit.exe filename -i position text");
+    puts("\tExample: textfile.txt -i 51 hello world");
 
-	puts("-d or --destroy:");
-	puts("\tLinux or Mac:");
-	puts("\t\t./iodit filename -d");
-	puts("\tWindows:");
-	puts("\t\tiodit.exe filename -d");
+    puts("-d or --destroy:");
+    puts("\tLinux or Mac:");
+    puts("\t\t./iodit filename -d");
+    puts("\tWindows:");
+    puts("\t\tiodit.exe filename -d");
 
-	puts("-s or --show:");
-	puts("\tLinux or Mac:");
-	puts("\t\t./iodit filename -s");
-	puts("\tWindows:");
-	puts("\t\tiodit.exe filename -s");
+    puts("-s or --show:");
+    puts("\tLinux or Mac:");
+    puts("\t\t./iodit filename -s");
+    puts("\tWindows:");
+    puts("\t\tiodit.exe filename -s");
+}
+
+void append(char *filename) {
+
+}
+
+void insert(char *filename) {
+
+}
+
+void destroy(char *filename) {
+    FILE *file;
+    if (access(filename, 0) == -1)
+    {
+        printf("%s File does not exist!\n", filename);
+    }
+    else {
+        if (access(filename, 2) != -1)
+        {
+            file = fopen(filename, "w+");
+            fclose(file);
+            printf("%s's text has been removed successfully!\n", filename);
+        }
+        else {
+            printf("%s text file does not have the Write Permission!\n", filename);
+        }
+    }
 }
 
 void show(char *filename) {
-	// We assume argv[1] is a filename to open
-        FILE *file = fopen( filename, "r" );
-
-        /* fopen returns 0, the NULL pointer, on failure */
-        if ( file == 0 )
-        {
-            printf( "Could not open file\n" );
+    if (access(filename, 0) == -1) {
+        printf("%s File does not exist!\n", filename);
+    }
+    else {
+        FILE *file = fopen(filename, "r");
+        if (file == 0) {
+            printf("Could not open file\n");
         }
-        else 
-        {
+        else {
             int x;
-            /* read one character at a time from file, stopping at EOF, which
-               indicates the end of the file.  Note that the idiom of "assign
-               to a variable, check the value" used below works because
-               the assignment statement evaluates to the value assigned. */
-            while  ( ( x = fgetc( file ) ) != EOF )
-            {
-                printf( "%c", x );
+            while ((x = fgetc(file)) != EOF) {
+                putchar(x);
             }
-            fclose( file );
+            fclose(file);
         }
+    }
 }
